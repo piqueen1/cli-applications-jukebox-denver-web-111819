ab40f41cbf61f29b46eaf97b0f42ff381b3fe6f6
Finn Thye <piqueen@gmail.com>
2019-08-22 02:44:08 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -27,14 +27,16 @@ def help
   puts "- exit : exits this program"
 end
 
-def play(song)
+def play(songs)
+  puts "Please enter a song name or number:"
+  chosen = gets.strip
   
+  if chosen.to_i && chosen.to_i < songs.count
+    name = songs[chosen.to_i]
+    elsif songs.include?(chosen)
+    name = chosen
+  end
 end
-
-songs =  [ "Phoenix - 1901", "Tokyo Police Club - Wait Up",
-"Sufjan Stevens - Too Much", "The Naked and the Famous - Young Blood", "(Far From) Home - Tiga", "The Cults - Abducted",
-"Phoenix - Consolation Prizes", 
-"Harry Chapin - Cats in the Cradle", "Amos Lee - Keep It Loose, Keep It Tight"]
 
 def list(songs)
   songs.each_with_index{ |song, i| 
