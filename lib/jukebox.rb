b776c96880b375c704d4c0c533dfdd14b70894bf
Finn Thye <piqueen@gmail.com>
2019-08-22 01:48:34 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,14 +20,30 @@ songs = [
 # puts say_hello(users_name)
 
 def help
-
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
-def play
+def play(song)
+  
 end
 
-def list
+songs =  [ "Phoenix - 1901", "Tokyo Police Club - Wait Up",
+"Sufjan Stevens - Too Much", "The Naked and the Famous - Young Blood", "(Far From) Home - Tiga", "The Cults - Abducted",
+"Phoenix - Consolation Prizes", 
+"Harry Chapin - Cats in the Cradle", "Amos Lee - Keep It Loose, Keep It Tight"]
+
+def list(songs)
+  songs.each_with_index{ |song, i| 
+    puts "#{i + 1}. #{song}"}
 end
+
+#
+# all numbers with index:
+# @numbers.each_with_index{|number, i| puts "#{i}: #{number}"}
 
 def exit_jukebox
 end
